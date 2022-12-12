8906670e7f7ad60e509d8c35b4572dbb70f2d245
Tom <tom.chabas@efrei.net>
2022-12-12 09:42:28 +0100

    c

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -7,7 +7,11 @@ import java.awt.event.ActionListener;
 import java.awt.Image;
 import java.util.Objects;
 
+
 public class Menu implements ActionListener {
+    static final int HEIGHT = 1080;
+    static final int WIDTH = 1920;
+
     JFrame frame= new JFrame(); //cadre graphique
 
     JButton boutonPrincipal =new JButton(); //bouton pour jouer
@@ -26,45 +30,22 @@ public class Menu implements ActionListener {
 
     Menu(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //cadre
-        frame.setSize(1920,1080); //taille du cadre
+        frame.setSize(WIDTH,HEIGHT); //taille du cadre
         frame.getContentPane().setBackground(new Color(65, 153, 187)); //couleur de fond
         frame.setLayout(null); //pour pas que ca bouge selon taille de l'ecran
         frame.setResizable(false); //on peut pas modifier taille fenetre
 
-        String lepath = "C:\\Users\\ilyes\\Downloads\\ProjetTransverseL22\\PRJTransverse\\src\\com\\company\\GIF\\";
+        String lepath = "";
 
         logo.setBounds(430, 100, 300, 300);
-        Icon FMS = new ImageIcon(new ImageIcon(lepath + "FMS.PNG").getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+        Icon FMS = new ImageIcon(new ImageIcon(lepath + "icon.PNG").getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
         logo.setIcon(FMS);
 
-        g1.setBounds(0, 0, 300, 300);
-        Icon imag = new ImageIcon(new ImageIcon(lepath + "cochon.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g1.setIcon(imag);
-        g2.setBounds(300, 0, 300, 300);
-        Icon imag2 = new ImageIcon(new ImageIcon(lepath + "crapeau.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g2.setIcon(imag2);
-        g3.setBounds(600, 0, 300, 300);
-        Icon imag3 = new ImageIcon(new ImageIcon(lepath + "chat.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g3.setIcon(imag3);
-        g4.setBounds(900, 0, 300, 300);
-        Icon imag4 = new ImageIcon(new ImageIcon(lepath + "cheval.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g4.setIcon(imag4);
-        g5.setBounds(0, 300, 300, 300);
-        Icon imag5 = new ImageIcon(new ImageIcon(lepath + "chien.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g5.setIcon(imag5);
-        g6.setBounds(300, 300, 300, 300);
-        Icon imag6 = new ImageIcon(new ImageIcon(lepath + "Football.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g6.setIcon(imag6);
-        g7.setBounds(600, 300, 300, 300);
-        Icon imag7 = new ImageIcon(new ImageIcon(lepath + "elephant.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g7.setIcon(imag7);
-        g8.setBounds(900, 300, 300, 300);
-        Icon imag8 = new ImageIcon(new ImageIcon(lepath + "escargot.gif").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        g8.setIcon(imag8);
+        
         
 
 
-        boutonPrincipal.setBounds(300, 500, 500, 100);
+        boutonPrincipal.setBounds(500, 600, 500, 100);
         boutonPrincipal.setText("Appuyer sur A pour démarrer");
         //boutonPrincipal.addActionListener(e -> {frame.dispose(); Quiz q = new MiniJeu1();});
         boutonPrincipal.addActionListener(buttonPressed);
